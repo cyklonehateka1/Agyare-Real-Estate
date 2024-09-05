bb6476c64bd94ae60cba2606502fa146b2c80697
Emmanuel <cyklonehateka1@gmail.com>
2024-09-05 02:15:46 +0000

    Updated responsive issue

--- src/pages/properties/components/Header.tsx
+++ src/pages/properties/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       style={{
         backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.84)), url(${bgImg})`,
       }}
-      className="bg-cover bg-center bg-no-repeat h-[27rem] sm:px-[10%] px-4 xs:px-6 pt-28 w-full flex flex-col justify-between relative"
+      className="bg-cover bg-center bg-no-repeat md:h-[27rem] h-[20rem] sm:px-[10%] px-4 xs:px-6 md:pt-28 pt-8 w-full flex flex-col justify-between relative"
     >
       <h1 className="text-white text-[2.5rem] sm:text-[4rem] font-bold leading-[4rem] w-[70%] mb-2">
         Properties
